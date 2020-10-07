c481d32a3770dd00232935d6a31326970fa727b5
tincogs <tomoday91@gmail.com>
2020-10-07 17:07:16 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        new_translation = PigLatinizer.new
+        @user_phrase = new_translation.piglatinize(params[:user_phrase])
+        erb :piglatinize
+    end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,17 @@
+class PigLatinizer
+    attr_reader :user_phrase
+
+    def piglatinize(input)
+        split_phrase = input.split(" ")
+        pigged = split_phrase.map do |word|
+            split_word = word.split(/([aAeEiIoOuU].*)/)
+            if split_word[0] == ""
+                split_word[1] + "way"
+            else
+                split_word[1] + split_word[0] + "ay"
+            end
+        end
+        pigged.join(" ")
+    end
+
+end
